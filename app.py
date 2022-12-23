4857b394711d3b1f6728a6b6277b013ccc444529
Josh Minchew <jminchew97@gmail.com>
2022-12-23 10:49:31 -0600

    can now add a store to stores list

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+stores = [
+    {
+        "name":"My Store",
+        "items": [
+            {
+                "name":"Chair",
+                "price":15.99
+            }
+        ]
+    }
+
+]
+
+@app.get("/store") #http://127.0.0.1:5000/store
+def get_stores():
+    return {"stores":stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name":request_data['name'], "items":[]}
+    stores.append(new_store)
+    return stores,201
