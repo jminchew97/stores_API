8c6ab97d605d9bd5e1f7e1ebd876dc72d06a8cbb
Josh Minchew <jminchew97@gmail.com>
2023-01-01 18:05:15 -0600

    -can now only enter unique items, no duplicates or returns 404
    -can now only enter unique store or returns 404

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from db import stores,items
 import uuid
+from flask_smorest import abort ## can use status code
 app = Flask(__name__)
 
 
@@ -12,25 +13,43 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(404, message="must contain store name")
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(404, message = "Error. Store already exists in DB.")
     store_id=uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
+    store = {**store_data, "id": store_id} #unpacks the dict with any info we have with **, then we add on an id after
+    stores[store_id] = store                # ** makes it more scalable as we add more key/values to stores
     return store,201
 
 # add new item to existing store
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+
+    if ("price" not in item_data 
+    or "name" not in item_data 
+    or "store_id" not in item_data):
+        abort(404, message="Bas request. Make sure 'price', 'name', and 'store_id' are in JSON payload")
+
+    # check if item already exists
+    for item in items.values():
+       if (item_data["name"] == item["name"] and 
+       item_data["store_id"]== item["store_id"]):
+           abort(404,message="item already exists")
+
     if item_data['store_id'] not in stores:
-        return {"message":"Invalid store_id"},404
+        abort(404,message="Store not found")
     
     # store ID is valid
     # create item UUID
     item_id =uuid.uuid4().hex
-    item = {**item_data,"item_id":item_id}
+    item = {**item_data,"item_id":item_id,"store_name":stores[item_data["store_id"]]}
     items[item_id] = item
 
-    return item, 201
+    return items, 201
 
 @app.get('/item')
 def get_all_items():
@@ -43,7 +62,7 @@ def get_store_info(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message":"Store ID not found"}, 404
+        abort(404,message="Store ID not found")
 
 # get specific store items
 @app.get("/items/<string:item_id>")
@@ -51,4 +70,4 @@ def get_store_items(item_id):
     try:
         return items['item_id']
     except KeyError:
-        return {"message":"item not found"}, 404
+        abort(404,message="Item not found")
